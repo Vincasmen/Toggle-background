550286fe9a67c029c1bc81c49777fc3d6878c19e
Vincasmen <vincasmen13@gmail.com>
2022-05-24 21:38:46 +0200

    Adding toggle functionality to another button and overlay those buttons.

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ const background = document.querySelector("body");
 
 let = toggleOff.classList.remove("hidden-off");
 
+//Click handler//
 toggleOff.addEventListener("click", function () {
   console.log("click");
-  toggle();
+  toggleOnBtn(), toggle(), toggleOffBtn();
 });
 
+toggleOn.addEventListener("click", function () {
+  console.log("click");
+  toggleOnBtn(), toggle(), toggleOffBtn();
+});
+
+//Toggle bacground to black//
 const toggle = function () {
   background.classList.toggle("white");
+};
+
+//Toggle switch button to be visible//
+const toggleOnBtn = function () {
+  toggleOn.classList.toggle("hidden-on");
+};
+
+const toggleOffBtn = function () {
+  toggleOff.classList.toggle("hidden-off");
 };
